c6ead3f4121fa337d7a5f0f261376de5429a0275
Hamidreza Khosroabadi <80723130+hamidrezakhosroabadi@users.noreply.github.com>
2023-04-29 09:15:15 -0700

    - Some methods were add to the ring class.

---
+++ hashring/ring.py
@@ -0,0 +1,38 @@
+"""This module contains SortedDict data structure."""
+from sortedcontainers import SortedDict
+
+
+class Ring:
+    """This class provides Ring class implementation."""
+
+    def __init__(self):
+        """This method inits the sorted dict data structure"""
+        self.__nodes = SortedDict()
+
+    def add_node(self, node):
+        """This method adds new nodes to sorted dict."""
+        self.__nodes[node.key] = node
+
+    def remove_node(self, node):
+        """This method removes nodes from sorted dict."""
+        del self.__nodes[node.key]
+
+    def get_node(self, key):
+        """This method retrives nodes from sorted dict by their keys."""
+        return self.__nodes[key]
+
+    def get_nodes(self):
+        """This method retrives all nodes from sorted dict."""
+        return self.__nodes
+
+    def find_route(self, key, count):
+        """This method finds the best destination for specefic key."""
+        return [self.__nodes[self.__nodes.iloc[self.__nodes.bisect(key+index)]] for index in range(count)]
+
+    def __len__(self):
+        """This method returns class length."""
+        return len(self.__nodes)
+
+    def __get_item__(self, key):
+        """This method returns class item by the key."""
+        return self.__nodes[key]
